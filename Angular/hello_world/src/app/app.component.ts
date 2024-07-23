1cfc12163b5a47edbc11db694a9d7071d837d7c1
dmitry-kuznets <dimkuz18@gmail.com>
2024-07-23 17:31:19 +0300

    Angular

--- Angular/hello_world/src/app/app.component.ts
+++ Angular/hello_world/src/app/app.component.ts
@@ -4,13 +4,15 @@ import { HeaderComponent } from "./mainLayout/header/header.component";
 import { FailedComponent } from "./mainLayout/failed/failed.component";
 import { SucceseComponent } from "./mainLayout/succese/succese.component";
 import { FormsModule } from '@angular/forms';
+import { MarkAttributeDirective } from './dir/mark-attribute.directive';
+import { NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, FailedComponent, SucceseComponent,FormsModule]
+    imports: [RouterOutlet, HeaderComponent, FailedComponent, SucceseComponent,FormsModule,MarkAttributeDirective,NgIf]
 })
 export class AppComponent {
   title = 'Hello World';
